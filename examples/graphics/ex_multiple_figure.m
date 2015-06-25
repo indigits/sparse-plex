253b3426560942c0cdd951c524dcef54e90d22ac
Shailesh Kumar <shailesh@indigits.com>
2015-06-25 14:43:26 +0530

    multi figures demo.

---
+++ examples/graphics/ex_multiple_figure.m
@@ -0,0 +1,12 @@
+width = 1024;
+height = 600;
+mf = SPX_Figures(width, height);
+
+mf.new_figure('Figure 1');
+
+mf.new_figure('Figure 2');
+
+mf.new_figure('Figure 3');
+
+mf.new_figure('Figure 4');
+
