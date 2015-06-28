e5842252db62ef7fcc666657c7293433fa237a83
Shailesh Kumar <shailesh@indigits.com>
2015-06-28 10:33:13 +0530

    sparse representation clustering

---
+++ examples/clustering/sparse_representation_clustering/ex_barbara_src_orth.m
@@ -0,0 +1,51 @@
+% Demonstrates the effectiveness of sparse representation clustering
+% on blocks from Barbara image using the orth dictionary from SPIE 2011
+% paper.
+
+clc;
+close all;
+clear all;
+problem = SPX_RecoveryProblems.problem_barbara_blocks();
+Phi = SPX_SimpleDicts.spie_2011('orth');
+K = 12;
+% the 8x8 blocks to be compressed.
+signals = problem.signals;
+signal_means = mean(signals);
+signals = bsxfun(@minus, signals, signal_means);
+% zero_mean_image = col2im(signals, [problem.blkSize, problem.blkSize], size(problem.image) , 'distinct');
+signals = signals(:, 1:100);
+tstart = tic;
+proxies = Phi.apply_ctranspose(signals);
+[d, s] = size(proxies);
+tmp = abs(proxies);
+[~, indices] = sort(tmp, 'descend');
+proxies2 = zeros(d, s);
+% Preserve appropriate part of the proxy
+for i=1:s
+    ind = indices(1:2*K, i);
+    proxies2(ind, i)  = proxies(ind,i);
+end
+% We identify the support for largest 2K entries
+%proxies(indices(5:end, :)) = 0;
+proxy_time = toc(tstart);
+NumClusters = 20;
+src = SPX_SparseRepClustering(proxies2, -1 , NumClusters);
+src.SpectralKMeansMaxIter = 1000;
+src.SpectralKMeansReplications  = 2;
+clustering_result = src.solve();
+elapsed_time = toc(tstart);
+fprintf('Sparse representation clustering time spent: %.2f seconds\n', elapsed_time);
+fprintf('Proxy time spent: %.8f seconds\n', proxy_time);
+fprintf('SVD time spent: %.8f seconds\n', src.Spectral.SVDTime);
+fprintf('K-means time spent: %.8f seconds\n', src.Spectral.KMeansTime);
+
+
+% Now perform full OMP recovery
+tstart = tic;
+omp = SPX_OrthogonalMatchingPursuit(Phi, K);
+omp.StopOnResidualNorm = false;
+omp.StopOnResNormStable = false;
+result = omp.solve_all_linsolve(signals);
+omp_elapsed_time = toc(tstart);
+representations = result.Z;
+fprintf('OMP recovery time spent: %.8f seconds\n', src.Spectral.KMeansTime);
---
+++ library/clustering/sparse_representation_clustering/SPX_SparseRepClustering.m
@@ -0,0 +1,98 @@
+classdef SPX_SparseRepClustering < handle
+    % Implements sparse representation clustering algorithm.
+    % Input: Representations of a set of signals in a known dictionary.
+    % Output: Clustering of signals
+
+    
+
+    properties
+        SpectralKMeansMaxIter = 1000
+        SpectralKMeansReplications = 5
+        Debug = false
+    end
+
+
+    properties(SetAccess=private)
+        % Dimension of representation space
+        N
+        % Number of signals in the space
+        S
+        % A matrix of size NxS where each column is one signal representation vector
+        Data
+        % The sparsity level or the largest dimension of the sparse subspaces
+        K
+        % The expected number of subspaces (could be unknown)
+        NumExpectedSubspaces
+        % Number of subspaces found
+        NumFoundSubspaces = -1
+        % Labels obtained through clustering
+        Labels
+        % Adjacency matrix
+        Adjacency
+        % The spectral clusterer
+        Spectral
+    end
+
+    methods
+        function self = SPX_SparseRepClustering(X, K, NumExpectedSubspaces)
+            % Constructor
+            if nargin < 3
+                NumExpectedSubspaces = -1;
+            end
+            if nargin < 2
+                K = -1;
+            end
+            self.Data = X;
+            self.K = K;
+            self.NumExpectedSubspaces = NumExpectedSubspaces;
+            [n, s] = size(X);
+            self.N = n;
+            self.S = s;
+            self.Labels = ones(s, 1);
+        end
+
+        function result = solve(self)
+            % prepare sparse representations
+            self.build_adjacency();
+            clusterer = SPX_SpectralClustering(self.Adjacency);
+            clusterer.Debug = self.Debug;
+            self.Spectral = clusterer;
+            clusterer.MAXiter = self.SpectralKMeansMaxIter;
+            clusterer.Replications  = self.SpectralKMeansReplications;
+            clusterer.NumClusters = self.NumExpectedSubspaces;
+            cluster_labels = clusterer.cluster_random_walk();
+            if self.Debug
+                cluster_labels'
+            end
+            self.Labels = cluster_labels;
+            self.NumFoundSubspaces = max(cluster_labels);
+            % Return the labels
+            result.Labels = self.Labels;
+            result.NumClusters = self.NumFoundSubspaces;
+        end
+
+    end
+
+    methods(Access=private)
+
+        function detect_outliers(self)
+            % Identifies outliers in the representations
+        end
+
+        function build_adjacency(self)
+            C = abs(self.Data); % N x S
+            % Normalize the matrix by column wise norms
+            C = SPX_Norm.normalize_l2(C);
+            % Compute inner product of signals with each other
+            innerProducts  = C' * C; % S x S.
+            % Since the signals are normalized, hence inner products are guaranteed to be 
+            % in the range of (0, 1)
+            % They can be used as similarity measurements.
+            % Keep it
+            self.Adjacency = innerProducts;
+        end
+
+    end
+
+end
+
