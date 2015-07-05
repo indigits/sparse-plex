86203cf9108f9123b5c62e5ad8318743fd213ffd
Shailesh Kumar <shailesh@indigits.com>
2015-07-05 18:53:46 +0530

    shifting of contents functions.

--- library/commons/SPX_Vec.m
+++ library/commons/SPX_Vec.m
@@ -50,6 +50,32 @@ classdef SPX_Vec
             end
         end
 
+        function y = shift_r(x)
+            % Right shift the contents of the vector
+            y = zeros(size(x));
+            y(2:end) = x(1:end-1);
+        end
+
+        function y = shift_rc(x)
+            % Circular right shift the contents of the vector
+            y = zeros(size(x));
+            y(2:end) = x(1:end-1);
+            y(1)  = x(end);
+        end
+
+
+        function y = shift_l(x)
+            % Left shift the contents of the vector
+            y = zeros(size(x));
+            y(1:end-1) = x(2:end);
+        end
+
+        function y = shift_lc(x)
+            % Circular left shift the contents of the vector
+            y = zeros(size(x));
+            y(1:end-1) = x(2:end);
+            y(end)  = x(1);
+        end
     end
 
 
--- library/commons/tests/test_spx_vector.m
+++ library/commons/tests/test_spx_vector.m
@@ -43,3 +43,40 @@ function test_reshape_as_proto
     y = SPX_Vec.reshape_as_prototype(x, p);
     assertEqual(x, y);
 end
+
+
+function test_shift_r
+    x = [1 2 3];
+    y = SPX_Vec.shift_r(x);
+    assertEqual(y, [0 1 2]);
+    x = x';
+    y = SPX_Vec.shift_r(x);
+    assertEqual(y, [0 1 2]');
+end
+
+function test_shift_rc
+    x = [1 2 3];
+    y = SPX_Vec.shift_rc(x);
+    assertEqual(y, [3 1 2]);
+    x = x';
+    y = SPX_Vec.shift_rc(x);
+    assertEqual(y, [3 1 2]');
+end
+
+function test_shift_l
+    x = [1 2 3];
+    y = SPX_Vec.shift_l(x);
+    assertEqual(y, [2 3 0]);
+    x = x';
+    y = SPX_Vec.shift_l(x);
+    assertEqual(y, [2 3 0]');
+end
+
+function test_shift_lc
+    x = [1 2 3];
+    y = SPX_Vec.shift_lc(x);
+    assertEqual(y, [2 3 1]);
+    x = x';
+    y = SPX_Vec.shift_lc(x);
+    assertEqual(y, [2 3 1]');
+end
