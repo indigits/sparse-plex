1f504911b249022154a4cbadbc369ad8166a62fa
Shailesh Kumar <shailesh@indigits.com>
2015-07-12 14:44:31 +0530

    some helper methods to check specific types of numbers.

--- library/discrete/SPX_Number.m
+++ library/discrete/SPX_Number.m
@@ -5,6 +5,35 @@ classdef SPX_Number
     end
     
     methods (Static)
+
+        function result = is_integer(value)
+            result = (mod(value, 1) == 0);
+        end
+
+        function result = is_positive_integer(value)
+            result = (value > 0) & (mod(value, 1) == 0);
+        end
+
+        function result = is_negative_integer(value)
+            result = (value < 0) & (mod(value, 1) == 0);
+        end
+
+        function result = is_odd(value)
+            result = (mod(value, 2) == 1);
+        end
+
+        function result = is_even(value)
+            result = (mod(value, 2) == 0);
+        end
+
+        function result = is_odd_natural(value)
+            result = (mod(value, 2) == 1) & (value > 0);
+        end
+
+        function result = is_even_natural(value)
+            result = (mod(value, 2) == 0) & (value > 0);
+        end
+
         function [a, b] =  findIntegerFactorsCloseToSquarRoot(n)
             % a cannot be greater than the square root of n
             % b cannot be smaller than the square root of n
