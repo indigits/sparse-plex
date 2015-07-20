db2c5004ef5e89d9b20f78bba776168fe75c73a9
Shailesh Kumar <shailesh@indigits.com>
2015-07-20 18:40:12 +0530

    apply a given transform to  a vector or a set of vectors.

--- library/signal_processing/SPX_SP.m
+++ library/signal_processing/SPX_SP.m
@@ -17,6 +17,41 @@ classdef SPX_SP
             locations = den * (1:count);
             locations = locations - (den -1);
         end
+
+        function y = apply_transform(x, transform, check_dyadic)
+            % Applies a given transform to a vector (row / column) or all columns of a signal matrix.
+            [n, d] = size(x);
+            if nargin < 3
+                check_dyadic = false;
+            end
+            if isvector(x)
+                n   = n * d;
+                if check_dyadic & ~SPX_Number.is_power_of_2(n)
+                    error('x must be of dyadic length.');
+                end
+                col = false;
+                if iscolumn(x)
+                    col = true;
+                    x = x';
+                end
+                y = transform(x, n);
+                if col
+                    % Convert the resulting vector to a column vector
+                    y = y';
+                end
+            else
+                % It is a signal matrix. We will process column by column
+                if check_dyadic & ~SPX_Number.is_power_of_2(n)
+                    error('signals must be of dyadic length.');
+                end
+                y = zeros(n, d);
+                for i=1:d
+                    y(:, i) = transform(x(:, i)', n);
+                end
+            end
+        end
+
+
     end
 
 
