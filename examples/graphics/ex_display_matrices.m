a7613ce28f9e6c544205f8beae2ec43aa7f18628
Shailesh Kumar <shailesh@indigits.com>
2015-07-26 14:42:04 +0530

    class changed.

--- examples/graphics/ex_display_matrices.m
+++ examples/graphics/ex_display_matrices.m
@@ -3,8 +3,8 @@ clear all; close all; clc;
 % The matrix to be displayed.
 data = spiral(100);
 
-SPX_Plot.matrix(data);
+SPX_Display.matrix(data);
 
 data = randn(100, 100);
 options.color_map = 'gray';
-SPX_Plot.matrix(data, options);
+SPX_Display.matrix(data, options);
