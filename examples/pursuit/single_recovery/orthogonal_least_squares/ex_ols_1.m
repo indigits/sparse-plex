8f3fe7902a02e1c9083703824a6885f42f88270a
Shailesh Kumar <shailesh@indigits.com>
2015-09-18 13:43:56 +0530

    Implementation of Orthogonal Least Squares and an example.

---
+++ examples/pursuit/single_recovery/orthogonal_least_squares/ex_ols_1.m
@@ -0,0 +1,45 @@
+close all;
+clear all;
+clc;
+rng('default');
+png_export = true;
+pdf_export = false;
+% Create the directory for storing images
+[status_code,message,message_id] = mkdir('bin');
+
+mf = SPX_Figures();
+
+% Signal space 
+N = 256;
+% Number of measurements
+M = 64;
+% Sparsity level
+K = 8;
+% Construct the signal generator.
+gen  = SPX_SparseSignalGenerator(N, K);
+% Generate bi-uniform signals
+x = gen.biUniform(1, 2);
+% Sensing matrix
+Phi = SPX_SimpleDicts.gaussian_dict(M, N);
+% Measurement vectors
+y = Phi.apply(x);
+% OMP MMV solver instance
+solver = SPX_OrthogonalLeastSquares(Phi, K);
+% Solve the sparse recovery problem
+result = solver.solve(y);
+% Solution vector
+z = result.z;
+
+mf.new_figure('OMP solution');
+subplot(411);
+stem(x, '.');
+title('Sparse vector');
+subplot(412);
+stem(z, '.');
+title('Recovered sparse vector');
+subplot(413);
+stem(abs(x - z), '.');
+title('Recovery error');
+subplot(414);
+stem(y, '.');
+title('Measurement vector');
