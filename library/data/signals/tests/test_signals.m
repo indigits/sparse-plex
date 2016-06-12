362d3204ec68b8f09ed422930e226396d87e00eb
Shailesh Kumar <shailesh@indigits.com>
2016-06-12 10:14:58 +0530

    fixing some unit tests

--- library/data/signals/tests/test_signals.m
+++ library/data/signals/tests/test_signals.m
@@ -1,18 +1,18 @@
-function test_suite = test_signals
-  initTestSuite;
+function tests = test_signals
+  tests = functiontests(localfunctions);
 end
 
-function test_picket_fence
+function test_picket_fence(testCase)
     s = SPX_SimpleSignals.picket_fence(16);
     expected_s = [ 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 ];
-    assertEqual(s, s);
+    verifyEqual(testCase, s, s);
     fs = fft(s);
-    assertEqual(SPX_Support.support(s), SPX_Support.support(fs));
+    verifyEqual(testCase, SPX_Support.support(s), SPX_Support.support(fs));
     Ns = (1:100).^2;
     for N=Ns
         s = SPX_SimpleSignals.picket_fence(N);
         fs = fft(s);
-        assertEqual(SPX_Support.support(s), SPX_Support.support(fs, 1e-8));
+        verifyEqual(testCase, SPX_Support.support(s), SPX_Support.support(fs, 1e-8));
     end    
 end
 
--- library/data/synthetic/tests/test_sparse_sig_gen.m
+++ library/data/synthetic/tests/test_sparse_sig_gen.m
@@ -1,9 +1,9 @@
-function test_suite = test_sparse_sig_gen
-  initTestSuite;
+function tests = test_sparse_sig_gen
+  tests = functiontests(localfunctions);
 end
 
 
-function test_uniform
+function test_uniform(testCase)
     N = 10;
     K = 4;
     S = 1;
@@ -13,12 +13,12 @@ function test_uniform
     result = gen.uniform(a, b);
     omega = gen.Omega;
     nz_part = result(omega);
-    assertTrue(all(nz_part <= b));
-    assertTrue(all(nz_part >= a));
+    verifyTrue(testCase, all(nz_part <= b));
+    verifyTrue(testCase, all(nz_part >= a));
 end
 
 
-function test_real_spherical_rows
+function test_real_spherical_rows(testCase)
     N = 10;
     K = 4;
     S = 20;
@@ -28,11 +28,11 @@ function test_real_spherical_rows
     nz_part = result(omega, :);    
     % verify that all rows have unit norm
     norms = SPX_Norm.norms_l2_rw(nz_part);
-    assertElementsAlmostEqual(norms, ones(K, 1));
+    verifyEqual(testCase, norms, ones(K, 1), 'AbsTol', 1e-6);
 end
 
 
-function test_cmplx_spherical_rows
+function test_cmplx_spherical_rows(testCase)
     N = 10;
     K = 4;
     S = 20;
@@ -42,5 +42,5 @@ function test_cmplx_spherical_rows
     nz_part = result(omega, :);    
     % verify that all rows have unit norm
     norms = SPX_Norm.norms_l2_rw(nz_part);
-    assertElementsAlmostEqual(norms, ones(K, 1));
+    verifyEqual(testCase, norms, ones(K, 1), 'AbsTol', 1e-6);
 end
