d64057648e81d053e6015cb8314a3d5df3df7701
Shailesh Kumar <shailesh@indigits.com>
2016-06-20 21:39:07 +0530

    reordering of data in hopkins files to ensure that points from same cluster are adjacent.

--- library/+spx/+data/+motion/Hopkins155.m
+++ library/+spx/+data/+motion/Hopkins155.m
@@ -1,5 +1,20 @@
 classdef Hopkins155 < handle
 
+% The variables inside the ground-truth file are organized as follow:
+% . width and height: dimensions (in pixels) of all the frames in the
+%   video sequence.
+% . points: number of tracked points P.
+% . frames: number of frames F.
+% . y: a matrix 3xPxF containing the homogeneous coordinates of the P
+%   points in the F frames.
+% . x: a matrix 3xPxF derived from y by normalizing the first two
+%   components of each vector such that they belong to the interval
+%   [-1;1].
+% . K: the 3x3 normalization matrix used to pass from y to x
+%   (x=K^(-1)*x).
+% . s: a Px1 vector containing the ground-truth segmentation; for each
+%   point it gives the index of the corresponding motion group.
+
 
 properties(SetAccess=private)
     root_dir
@@ -74,9 +89,31 @@ methods
             data = load(path);
             % let's fill up more info
             s = data.s;
+            % ensure that s is a row vector
+            if iscolumn(s)
+                s = s';
+            end
             result.num_clusters = max(s);
-            result.labels = s;
+            % let us work through the labels to reorder them
+            % such that points from each subspace are adjacent.
+            mapping = [];
+            counts = [];
+            labels = [];
+            for k=1:result.num_clusters
+                % entries for k-th cluster
+                entries = find(s == k);
+                mapping = [mapping entries];
+                count = length(entries);
+                counts = [counts count];
+                labels = [labels k*ones(1, count)];
+            end
+            % labels of points
+            result.labels = labels;
+            % counts of each cluster
+            result.counts = counts;
             x = data.x;
+            % reorder x
+            x = x(:, mapping, :);
             % number of points
             S = size(x, 2);
             % number of frames
@@ -87,7 +124,13 @@ methods
             M = 2 * F;
             result.M  = M;
             % reshape the data in x for clustering purpose
-            X = reshape(permute(x(1:2,:,:),[1 3 2]),M,S);
+            % drop the third component of each point in x [homogeneous part]
+            x = x(1:2,:,:);
+            % now x is 2xPxF (P : points, F is frames)
+            % bring frames dimension ahead and points dimension behind
+            x = permute(x,[1 3 2]);
+            % Now map the 2 coordinates of each point in each column
+            X = reshape(x,M,S);
             result.X = X;
             % clean up unnecessary fields
             %fields = {'x', 'y', 's', 'width', 'height', 'frames', 'K'};
