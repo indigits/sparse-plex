0a1246a598e495d0cc6e1122cf3c4cc15969fd53
Shailesh Kumar <shailesh@indigits.com>
2016-06-20 13:59:58 +0530

    file system API moved out

--- library/+spx/+commons/core.m
+++ library/+spx/+commons/core.m
@@ -2,20 +2,6 @@ classdef core
 % General utility functions
 methods(Static)
 
-function result = is_directory(dirpath)
-    % Returns if dirpath is a directory or not.
-    result = exist(dirpath, 'dir') ~= 0;
-end
-
-function result = is_file_or_dir(filepath)
-    % Returns true if filepath is either a file or a directory
-    result = (exist(filepath, 'file') ~= 0);
-end
-
-function result = is_file(filepath)
-    % Returns true if filepath is a file
-    result = (exist(filepath, 'file') ~= 0) & (exist(filepath, 'dir') == 0);
-end
 
 function result = is_class(classpath)
     % Returns true if classpath points to a MATLAB class.
