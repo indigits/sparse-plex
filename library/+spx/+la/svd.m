0ac2b4dcbb0b016c777dddfbbe54f358fd6a5589
Shailesh Kumar <shailesh@indigits.com>
2016-06-20 19:44:55 +0530

    vidal's rank method

--- library/+spx/+la/svd.m
+++ library/+spx/+la/svd.m
@@ -9,6 +9,24 @@ methods(Static)
         max_gap = -min_val;
     end
 
+    function rank = vidal_rank(singular_values,kappa)
+        % Rank used in Vidal papers
+        n = length(singular_values);
+        % we will try rank values between 1 to n-1.
+        % an array to store criterion value
+        criterion = zeros(1, n-1);
+        for(r=1:n-1)
+            num = singular_values(r+1)^2;
+            den = sum(singular_values(1:r).^2);
+            criterion(r)=num/den + kappa*r;
+        end
+        %criterion
+        % find the rank with minimum value of the criterion
+        [min_value, index]=min(criterion);
+        rank = index;
+    end
+
+
 end
 
 end
--- library/tests/la/test_svd.m
+++ library/tests/la/test_svd.m
@@ -15,3 +15,24 @@ function test_mahdi_rank(testCase)
     verifyEqual(testCase, g, .4, 'AbsTol', 1e-12);
 
 end
+
+function test_vidal_rank(testCase)
+    s = [ 1 1 1 1  0  0];
+    for kappa=0:.01:.24
+        r = spx.la.svd.vidal_rank(s, kappa);
+        verifyEqual(testCase, r, 4);
+    end
+
+    s = [ 1 .9 .8 .7  .3  .2 .1];
+    for kappa=0.02:0.01:.16
+        r = spx.la.svd.vidal_rank(s, kappa);
+        % fprintf('%f, %d\n', kappa, r);
+        verifyEqual(testCase, r, 4);
+    end
+    for kappa=0.17:0.01:2
+        r = spx.la.svd.vidal_rank(s, kappa);
+        % fprintf('%f, %d\n', kappa, r);
+        verifyNotEqual(testCase, r, 4);
+    end
+
+end
