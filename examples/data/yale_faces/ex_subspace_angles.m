c0a23ddaea4450178c3be49971e8c0767422e2b1
Shailesh Kumar <shailesh@indigits.com>
2016-06-21 21:31:26 +0530

    better calculation of subspace angles

--- examples/data/yale_faces/ex_subspace_angles.m
+++ examples/data/yale_faces/ex_subspace_angles.m
@@ -8,15 +8,21 @@ yf.load();
 
 ns = yf.num_subjects();
 ni = yf.ImagesToLoadPerSubject;
-fprintf('Computing angles between subspaces');
 tstart = tic;
 Y = yf.ImageData;
 means = mean(Y);
 %Y = bsxfun(@minus, Y, means);
-
-angles = spx.la.spaces.smallest_angles_deg(Y, ni);
+fprintf('Computing bases:\n');
+bases = spx.la.svd.low_rank_bases(Y, ni * ones(1, ns), 10);
+% [bases, ranks]  = spx.la.svd.vidal_rank_bases(Y, ni * ones(1, ns),0.001);
+% fprintf('Ranks: ');
+% fprintf('%d ', ranks);
+% fprintf('\n');
+fprintf('Computing angles between subspaces:');
+angles = spx.la.spaces.smallest_angles_deg(bases);
+% angles = spx.la.spaces.smallest_angles_deg(Y, ni);
 elapsed = toc(tstart);
-off_diag_angles = SPX_Mat.off_diag_upper_tri_elements(angles);
+off_diag_angles = spx.commons.matrix.off_diag_upper_tri_elements(angles);
 off_diag_angles = sort(off_diag_angles);
 minimum_angle = min(off_diag_angles);
 maximum_angle = max(off_diag_angles);
