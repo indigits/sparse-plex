0a433d53a509aac49420639dc78e1344f36976b4
Shailesh Kumar <shailesh@indigits.com>
2016-06-22 19:06:27 +0530

    name mistake fixed

--- examples/clustering/subspace_clustering/noise_test/ssc_l1_mahdi.m
+++ examples/clustering/subspace_clustering/noise_test/ssc_l1_mahdi.m
@@ -1,3 +1,3 @@
-function result = ssc_nn_omp(X, D, K)
+function result = ssc_l1_mahdi(X, D, K)
     result = spx.cluster.subspace.ssc_l1_mahdi(X);
 end
--- examples/clustering/subspace_clustering/subspace_preservation_test/ssc_l1_mahdi.m
+++ examples/clustering/subspace_clustering/subspace_preservation_test/ssc_l1_mahdi.m
@@ -1,4 +1,4 @@
-function result = ssc_nn_omp(X, D, K)
+function result = ssc_l1_mahdi(X, D, K)
     options.num_clusters = K;
     result = spx.cluster.subspace.ssc_l1_mahdi(X, options);
 end
