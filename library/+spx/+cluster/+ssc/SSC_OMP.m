c0bc8b6daca9b07e2f1d6b102c55af3fd220d13b
Shailesh Kumar <shailesh@indigits.com>
2016-07-03 15:37:04 +0530

    impleemntation revamped for speed.

--- library/+spx/+cluster/+ssc/SSC_OMP.m
+++ library/+spx/+cluster/+ssc/SSC_OMP.m
@@ -24,10 +24,14 @@ classdef SSC_OMP < handle
         Adjacency
         % The spectral clusterer used in the algorithm
         Clusterer
+        % Threshold for residual norm
+        ResidualNormThreshold
+        % Number of iterations for each vector
+        Iterations
     end
 
     methods
-        function self = SSC_OMP(X, K, NumSubspaces)
+        function self = SSC_OMP(X, K, NumSubspaces, ResidualNormThreshold)
             % Constructor
             self.Data = X;
             self.K = K;
@@ -35,6 +39,10 @@ classdef SSC_OMP < handle
                 NumSubspaces = -1;
             end
             self.NumSubspaces = NumSubspaces;
+            if nargin < 4
+                ResidualNormThreshold = 1e-3;
+            end
+            self.ResidualNormThreshold = ResidualNormThreshold;
             [n, s] = size(X);
             self.N = n;
             self.S = s;
@@ -45,27 +53,26 @@ classdef SSC_OMP < handle
 
         function result = solve(self)
             % prepare sparse representations
+            tstart = tic;
             self.recover_coefficients();
+            representation_time = toc(tstart);
             self.build_adjacency();
 
             % conduct spectral clustering
-            options.num_clusters = self.NumSubspaces;
-            result = spx.cluster.spectral.simple.normalized_symmetric(self.Adjacency, options);
+            tstart = tic;
+            result = spx.cluster.spectral.simple.normalized_symmetric_sparse(self.Adjacency, self.NumSubspaces);
             cluster_labels = result.labels;
-
+            result.clustering_time = toc(tstart);
             % We are disabling our version of spectral clustering for now.            
             % clusterer = spx.cluster.spectral.Clustering(self.Adjacency);
             % keep reference for debugging purposes.
             % self.Clusterer = clusterer;
             % clusterer.NumClusters = self.NumSubspaces;
-            % cluster_labels = clusterer.cluster_random_walk();
-
-            
+            % cluster_labels = clusterer.cluster_random_walk();            
             self.Labels = cluster_labels;
-            % Return the labels
-            result.Labels = self.Labels;
             result.Z = self.Representation;
             result.W = self.Adjacency;
+            result.representation_time = representation_time;
         end
 
     end
@@ -74,33 +81,100 @@ classdef SSC_OMP < handle
 
         function recover_coefficients(self)
             % Computes sparse representations of the data vectors
-            data = self.Data;
-            data = spx.commons.norm.normalize_l2(data);
+            data_matrix = self.Data;
+            data_matrix = spx.commons.norm.normalize_l2(data_matrix);
             % Number of data vectors
             ns = self.S;
-            % iterate over signals
-            all_cols = 1:ns;
-            for s=all_cols
+            % sparsity level
+            nk = self.K;
+            % support set for each vector [one row for each vector]
+            support_sets = ones(ns, nk);
+
+            % termination vector
+            % contains the number of iterations in each a particular vector was solved.
+            % initially it is set to K
+            % the moment residual norm reduces below the threshold
+            % we mark it as finished and store the number of iterations
+            % in this vector
+            termination_vector = nk * ones(ns, 1);
+
+            % the normalized data is the starting point of vector
+            residual_matrix  = data_matrix;
+            % threshold for squared norm of residual 
+            threshold = self.ResidualNormThreshold^2;
+            for iter=1:nk
                 if ~self.Quiet 
-                    if mod(s, 50) == 0
-                        fprintf('.');
+                    fprintf('.');
+                end
+                % We correlated data with residual.
+                % to do change this to handle cases where there are too many points to handle
+                correlation_matrix = abs(data_matrix' * residual_matrix); % S x M  times M x S = S x S
+                % set all the diagonal entries (inner product with self) to zero.
+                correlation_matrix(1:ns+1:end) = 0;
+                % the inner product of a residual with each atom is stored along a column
+                % we need to take maximum of each column to identity the best matching atom
+                [~, best_match_indices] = max(correlation_matrix, [], 1);
+                % we fill in the support set
+                support_sets(:, iter) = best_match_indices;
+                if (iter ~= nk)
+                    % we need to compute residuals except for the last iteration.
+                    % iterate for each data point
+                    for s=1:ns
+                        if termination_vector(s) == nk
+                            % this vector requires more iterations.
+                            % pick the vector
+                            x = data_matrix(:, s);
+                            % pick support for this vector
+                            support_set = support_sets(s, 1:iter);
+                            % pick atoms
+                            submatrix = data_matrix(:, support_set);
+                            % solve the least squares problem
+                            r = x - submatrix * (submatrix \ x);
+                            % put the new residual into residual matrix
+                            residual_matrix(:, s) = r;
+                            if sum(r.^2) < threshold
+                                % The processing of this vector is complete
+                                termination_vector(s) = iter;
+                            end
+                        end
                     end
-                    if mod(s, 1000) == 0
-                        fprintf('\n');
+                    % now check if processing for all vectors have been terminated
+                    continuing = termination_vector == nk;
+                    if sum(continuing) == 0
+                        % all vectors have been terminated
+                        break;
                     end
                 end
-                % s-th data vector
-                x = data(:, s);
-                % Prepare the l1 solver
-                solver = spx.pursuit.single.OrthogonalMatchingPursuit(data, self.K);
-                solver.Verbose = false;
-                solver.IgnoredAtom = s;
-                % Run the solver to obtain sparse representation
-                result = solver.solve(x);
-                % The obtained representation is in R^{S - 1} dimensions
-                % Put back the representation
-                self.Representation(:, s) = result.z;
             end
+            % final computation of coefficients
+            % the values of coefficients corresponding to support sets
+            coefficients_matrix = zeros(ns, nk);
+            % Creation of sparse representation matrix
+            for s=1:ns
+                % number of iterations for this vector
+                k = termination_vector(s);
+                support_set  = support_sets(s, 1:k);
+                x = self.Data(:, s);
+                % pick atoms from the unnormalized data matrix
+                submatrix = self.Data(:, support_set);
+                % solve the least squares problem
+                coeff = submatrix \ x;
+                % put the coefficients back into coefficients matrix
+                coefficients_matrix(s, 1:k) = coeff';
+            end
+
+            % each column varies from 1 to ns
+            % each row is just a repetition
+            column_indices  = repmat((1:ns)', 1, nk);
+
+            % the support set, column_indices and coefficients_matrix are combined to form
+            % the representation matrix at the end of function as follows
+            % the support set contains row number
+            % the column_indices contains the column number
+            % the values contains the value of the non-zero entry
+            % ns, ns is the size of the sparse matrix.
+            self.Representation = sparse( support_sets(:), column_indices(:), coefficients_matrix(:), ns, ns);
+            self.Iterations = termination_vector;
             if ~self.Quiet 
                 fprintf('\n');
             end
@@ -114,7 +188,7 @@ classdef SSC_OMP < handle
         function build_adjacency(self)
             C = abs(self.Representation);
             % Normalize the matrix by column wise maximums
-            C = spx.commons.norm.normalize_linf(C);
+            % C = spx.commons.norm.normalize_linf(C);
             % Make it symmetric
             C = C + C';
             % Keep it
