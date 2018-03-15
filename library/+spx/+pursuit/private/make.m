595a3bea983c2cdd9083020790b708275a5f45dc
Shailesh Kumar <shailesh@indigits.com>
2018-03-16 00:25:23 +0530

    mex compilation code

--- library/+spx/+pursuit/private/make.m
+++ library/+spx/+pursuit/private/make.m
@@ -9,23 +9,30 @@ if (is64bit)
   compile_params{end+1} = '-largeArrayDims';
 end
 compile_params{end+1} = '-lmwblas';
+compile_params{end+1} = '-lmwlapack';
 
 
 % Compile files %
-sources = {'argcheck.c', 'spxblas.c'};
+sources = {'argcheck.c', 'spxblas.c', 'spxalg.c', 'omp.c'};
 % ,'ompcore.c','omputils.c','myblas.c','ompprof.c'
 
-disp('Compiling ompmex...');
-mex('mex_omp.c', sources{:},compile_params{:});
+% disp('Compiling ompmex...');
+% mex('mex_omp.c', sources{:},compile_params{:});
 
-disp('Compiling mex_mult_mat_vec...');
-mex('mex_mult_mat_vec.c', sources{:},compile_params{:});
+% disp('Compiling mex_mult_mat_vec...');
+% mex('mex_mult_mat_vec.c', sources{:},compile_params{:});
 
-disp('Compiling mex_mult_mat_t_vec...');
-mex('mex_mult_mat_t_vec.c', sources{:},compile_params{:});
+% disp('Compiling mex_mult_mat_t_vec...');
+% mex('mex_mult_mat_t_vec.c', sources{:},compile_params{:});
 
-disp('Compiling mex_mult_mat_mat...');
-mex('mex_mult_mat_mat.c', sources{:},compile_params{:});
+% disp('Compiling mex_mult_mat_mat...');
+% mex('mex_mult_mat_mat.c', sources{:},compile_params{:});
 
-disp('Compiling mex_mult_mat_t_mat...');
-mex('mex_mult_mat_t_mat.c', sources{:},compile_params{:});
+% disp('Compiling mex_mult_mat_t_mat...');
+% mex('mex_mult_mat_t_mat.c', sources{:},compile_params{:});
+
+% disp('Compiling mex_test_blas...');
+% mex('mex_test_blas.c', sources{:},compile_params{:});
+
+disp('Compiling mex_omp_chol...');
+mex('mex_omp_chol.c', sources{:},compile_params{:});
