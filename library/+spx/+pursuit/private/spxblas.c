9308d14bd463142c33de3a6a8ad3e480e0ed5878
Shailesh Kumar <shailesh@indigits.com>
2018-03-16 00:24:06 +0530

    reorg of blas code and bug fixes.

--- library/+spx/+pursuit/private/spxblas.c
+++ library/+spx/+pursuit/private/spxblas.c
@@ -1,5 +1,7 @@
 #include "spxblas.h"
 #include "blas.h"
+#include "lapack.h"
+#include "stddef.h"
 
 
 /**
@@ -17,11 +19,118 @@ Some useful references:
 #endif
 
 
+/********************************************
+* One vector operations
+*
+*********************************************/
+
+mwIndex abs_max_index(const double x[], mwSize n){
+    mwSignedIndex  nn = n;
+    mwSignedIndex  incx = 1;
+    return idamax(&nn, x, &incx) - 1;
+}
+
+
+mwIndex max_index(const double x[], mwSize n){
+    mwIndex maxid=0, k;
+    double val, maxval = *x;
+
+    for (k=1; k<n; ++k) {
+        val = x[k];
+        if (val > maxval) {
+            maxval = val;
+            maxid = k;
+        }
+    }
+    return maxid;    
+}
+
+void vec_extract(const double x[], 
+    const mwIndex indices[], double y[], 
+    mwSize k){    
+    
+    mwIndex i;
+
+    for (i=0; i<k; ++i){
+        y[i] = x[indices[i]];
+    }
+}
+
+void vec_set_value(double x[], double value, mwSize n){
+    int i;
+    for (i=0; i < n; ++i){
+        x[i] = value;
+    }
+}
+
+
+/********************************************
+* One matrix operations
+*
+*********************************************/
+
+/* matrix transpose */
+
+void mat_transpose(const double X[], double Y[], mwSize n, mwSize m)
+{
+  mwIndex i, j, i_m, j_n;
+  
+  if (n<m) {
+    for (j=0; j<m; ++j) {
+      j_n = j*n;
+      for (i=0; i<n; ++i) {
+        Y[j+i*m] = X[i+j_n];
+      }
+    }
+  }
+  else {
+    for (i=0; i<n; ++i) {
+      i_m = i*m;
+      for (j=0; j<m; ++j) {
+        Y[j+i_m] = X[i+j*n];
+      }
+    }
+  }
+}
+
+
+/********************************************
+* Vector vector operations
+*
+*********************************************/
+
+void sum_vec_vec(double alpha, const double x[], double y[], mwSize n){
+    mwSignedIndex  nn = n;
+    mwSignedIndex  incx = 1; 
+    daxpy(&nn, &alpha, x, &incx, y, &incx);   
+}
+
+void copy_vec_vec(const double x[], double y[], mwSize n){
+    mwSignedIndex  nn = n;
+    mwSignedIndex  inc = 1; 
+    dcopy(&nn, x, &inc, y, &inc);
+}
+
+
+double inner_product(const double a[], 
+    const double b[], mwSize n){
+    mwSignedIndex  nn = n;
+    mwSignedIndex  inc = 1; 
+    ddot(&nn, a, &inc, b, &inc);
+}
+
+
+
+/********************************************
+* Matrix vector operations
+*
+*********************************************/
+
 
 void mult_mat_vec(double alpha, 
     const double A[], 
     const double x[], 
-    double y[], size_t m, size_t n){
+    double y[], mwSize m, mwSize n){
     
     mwSignedIndex  incx = 1;
     mwSignedIndex  incy = 1;
@@ -36,7 +145,7 @@ void mult_mat_vec(double alpha,
 void mult_mat_t_vec(double alpha, 
     const double A[], 
     const double x[], 
-    double y[], size_t m, size_t n){
+    double y[], mwSize m, mwSize n){
     
     mwSignedIndex  incx = 1;
     mwSignedIndex  incy = 1;
@@ -48,11 +157,137 @@ void mult_mat_t_vec(double alpha,
         &beta, y, &incy);
 }
 
+void mult_mat_vec_sp(double alpha, 
+    const double A[], 
+    const  double pr[], 
+    const mwIndex ir[], const mwIndex jc[],
+    double y[], mwSize m, mwSize n){
+    mwIndex i, j, j_n, k, kend;
+    /**
+    Initialize result to 0
+    */
+    for (i=0; i<m; ++i) {
+        y[i] = 0;
+    }
+
+    kend = jc[1];
+    if (kend==0) {   /* x is empty */
+        return;
+    }
+
+    for (k=0; k<kend; ++k) {
+        j = ir[k];
+        j_n = j*n;
+        /**
+        y += x[k] * A[j]
+        */
+        sum_vec_vec(alpha * pr[k], A + j_n, y, m);
+    }
+}
+
+
+void lt_back_substitution(const double L[], 
+    const double b[], 
+    double x[], 
+    mwSize m, mwSize k)
+{
+  mwIndex i, j;
+  double rhs;
+
+  /**
+  Iterate over rows
+  */
+  for (i=0; i<k; ++i) {
+    rhs = b[i];
+    /**
+    Subtract over the entries in i-th row before
+    the diagonal element.
+    */
+    for (j=0; j<i; ++j) {
+      rhs -= L[j*m+i]*x[j];
+    }
+    /**
+    Divide by the value in the i-th diagonal entry.
+    */
+    x[i] = rhs/L[i*m+i];
+  }
+}
+
+
+void  ut_back_substitution(const double U[], 
+    const double b[], 
+    double x[], 
+    mwSize m, mwSize k){
+    mwIndex i, j;
+    double rhs;
+
+    /** We start from last row */ 
+    for (i=k; i>=1; --i) {
+        rhs = b[i-1];
+        /** Subtract contributions from entries in row */
+        for (j=i; j<k; ++j) {
+          rhs -= U[j*m+i-1]*x[j];
+        }
+        /** Divide from the diagonal element in (i-1)-th row */
+        x[i-1] = rhs/U[(i-1)*m+i-1];
+      }
+}
+
+/**
+Solves L' x = b
+*/
+void lt_t_back_substitution(const double L[], 
+    const double b[], 
+    double x[], 
+    mwSize m, mwSize k){
+
+    mwIndex i, j;
+    double rhs;
+
+    for (i=k; i>=1; --i) {
+        rhs = b[i-1];
+        for (j=i; j<k; ++j) {
+            rhs -= L[(i-1)*m+j]*x[j];
+        }
+        x[i-1] = rhs/L[(i-1)*m+i-1];
+    }
+
+}
+
+
+void spd_chol_lt_solve(const double L[], 
+    const double b[], 
+    double x[], 
+    mwSize m, mwSize k){
+
+    double *tmp;
+    tmp = mxMalloc(k*sizeof(double));
+    /**
+    Solve the problem L t = b
+    */
+    lt_back_substitution(L, b, tmp, m, k);
+    /**
+    Solve the problem L' x = t
+    */
+    lt_t_back_substitution(L, tmp, x, m, k);
+    mxFree(tmp);
+}
+
+
+
+
+/********************************************
+* Matrix matrix operations
+*
+*********************************************/
+
+
+
 void mult_mat_mat(double alpha, 
     const double A[], 
     const double B[], 
     double X[], 
-    size_t m, size_t n, size_t k){
+    mwSize m, mwSize n, mwSize k){
     char atrans = 'N';
     char btrans = 'N';
     double beta = 0;
@@ -72,7 +307,7 @@ void mult_mat_t_mat(double alpha,
     const double A[], 
     const double B[], 
     double X[], 
-    size_t m, size_t n, size_t k){
+    mwSize m, mwSize n, mwSize k){
     char atrans = 'T';
     char btrans = 'N';
     double beta = 0;
@@ -88,4 +323,53 @@ void mult_mat_t_mat(double alpha,
         X, &mm);
 }
 
+
+
+
+
+/********************************************
+* Debugging functions
+*
+*********************************************/
+
+
+
+
+/* print contents of matrix */
+
+void print_matrix(const double A[], int n, int m, char* matrix_name)
+{
+  int i, j;
+  mexPrintf("\n%s = \n\n", matrix_name);
+
+  if (n*m==0) {
+    mexPrintf("   Empty matrix: %d-by-%d\n\n", n, m);
+    return;
+  }
+
+  for (i=0; i<n; ++i) {
+    for (j=0; j<m; ++j)
+      mexPrintf("   %lf", A[j*n+i]);
+    mexPrintf("\n");
+  }
+  mexPrintf("\n");
+}
+
+/* print contents of sparse vector */
+
+void print_sparse_vector(const mxArray *A, char* vector_name)
+{
+  mwIndex *aJc = mxGetJc(A);
+  mwIndex *aIr = mxGetIr(A);
+  double *aPr = mxGetPr(A);
+
+  int i;
+
+  mexPrintf("\n%s = \n\n", vector_name);
+
+  for (i=0; i<aJc[1]; ++i){
+    printf("   (%d,1) = %lf\n", aIr[i]+1,aPr[i]);
+  }
+  mexPrintf("\n");
+}
 
--- library/+spx/+pursuit/private/spxblas.h
+++ library/+spx/+pursuit/private/spxblas.h
@@ -1,5 +1,20 @@
 /**
 Wrapper for BLAS functions
+
+This code is derived from OMPBOX.
+Hand coded C routines have been replaced
+in many cases with implementations from
+BLAS and LAPACK.
+
+Some conventions:
+- input arguments are marked with const
+- output arguments are non-const
+- array values are returned  through output arguments
+- scalar values are returned through return values.
+- input arrays come first.
+- output arrays follow.
+- scalars describing dimensions etc. come last.
+
 */
 
 #ifndef _SPX_BLAS_H_
@@ -13,6 +28,73 @@ Square of a value
 */
 #define SQR(x) ((x) * (x))
 
+/********************************************
+* One vector operations
+*
+*********************************************/
+
+/**
+Returns the index of the maximum absolute value in the array
+*/
+mwIndex abs_max_index(const double x[], mwSize n);
+
+
+
+/**
+Returns the index of the maximum value in the array
+*/
+mwIndex max_index(const double x[], mwSize n);
+
+/**
+Extracts a vector y = x(indices)
+*/
+void vec_extract(const double x[], 
+    const mwIndex indices[], double y[], mwSize k);
+
+/**
+Set same value to all entries in vector
+*/
+void vec_set_value(double x[], double value, mwSize n);
+
+/********************************************
+* One matrix operations
+*
+*********************************************/
+
+/**
+Computes the transpose
+Y = X'
+*/
+void mat_transpose(const double X[], double Y[], mwSize m, mwSize n);
+
+
+/********************************************
+* Vector vector operations
+*
+*********************************************/
+
+/**
+Computes 
+y = alpha * x + y 
+*/
+void sum_vec_vec(double alpha, const double x[], double y[], mwSize n);
+
+/***
+Copies a vector y = x
+*/
+void copy_vec_vec(const double x[], double y[], mwSize n);
+
+
+/**
+Computes inner product of two vectors
+*/
+double inner_product(const double a[], const double b[], mwSize n);
+
+
+/********************************************
+* Matrix vector operations
+*
+*********************************************/
 
 /***
 Compute y = alpha * A * x
@@ -20,7 +102,7 @@ Compute y = alpha * A * x
 void mult_mat_vec(double alpha, 
     const double A[], 
     const double x[], 
-    double y[], size_t m, size_t n);
+    double y[], mwSize m, mwSize n);
 
 /***
 Compute y = alpha * A' * x
@@ -28,7 +110,121 @@ Compute y = alpha * A' * x
 void mult_mat_t_vec(double alpha, 
     const double A[], 
     const double x[], 
-    double y[], size_t m, size_t n);
+    double y[], mwSize m, mwSize n);
+
+/***
+Compute y = alpha * A * x where x is sparse array
+*/
+void mult_mat_vec_sp(double alpha, 
+    const double A[], 
+    const double pr[], const mwIndex ir[], const mwIndex jc[],
+    double y[], mwSize m, mwSize n);
+
+
+
+
+/**
+Solves a lower triangular system via back substitution
+
+L x  = b
+
+where L is lower triangular.
+
+L can be of a size m x n where 
+m is the number of rows and n
+is the number of columns.
+
+k is the number of equations to solve.
+We require that k <= m, n
+
+Only the  lower triangle of the submatrix L(1:k, 1:k) is used 
+for solving the problem.
+
+*/
+void lt_back_substitution(const double L[], 
+    const double b[], 
+    double x[], 
+    mwSize m, mwSize k);
+
+/**
+Solves an upper triangular system via back substitution
+
+U x  = b
+
+where U is upper triangular.
+
+U can be of a size m x n where 
+m is the number of rows and n
+is the number of columns.
+
+k is the number of equations to solve.
+We require that k <= m, n
+
+Only the upper triangle of the submatrix U(1:k, 1:k) is used 
+for solving the problem.
+
+*/
+void ut_back_substitution(const double U[], 
+    const double b[], 
+    double x[], 
+    mwSize m, mwSize k);
+
+/**
+Solves an upper triangular system via back substitution
+
+L' x  = b
+
+where L is lower triangular.
+
+L can be of a size m x n where 
+m is the number of rows and n
+is the number of columns.
+
+k is the number of equations to solve.
+We require that k <= m, n
+
+Only the upper triangle of the submatrix L'(1:k, 1:k) is used 
+for solving the problem.
+
+*/
+void lt_t_back_substitution(const double L[], 
+    const double b[], 
+    double x[], 
+    mwSize m, mwSize k);
+
+
+/**
+Solves the linear system 
+A x = b
+
+where A is a symmetric positive definite matrix
+with the decomposition
+A = L L' where L is a lower triangular matrix.
+
+L can be of a size m x n where 
+m is the number of rows and n
+is the number of columns.
+
+k is the number of equations to solve.
+We require that k <= m, n
+
+Only the  lower triangle of the submatrix L(1:k, 1:k) is used 
+for solving the problem.
+*/
+void spd_chol_lt_solve(const double L[], 
+    const double b[], 
+    double x[], 
+    mwSize m, mwSize k);
+
+
+/********************************************
+* Matrix matrix operations
+*
+*********************************************/
+
+
+
+
 
 /** 
 Matrix matrix multiplication 
@@ -41,7 +237,7 @@ void mult_mat_mat(double alpha,
     const double A[], 
     const double B[], 
     double X[], 
-    size_t m, size_t n, size_t k);
+    mwSize m, mwSize n, mwSize k);
 
 /** 
 Matrix transpose matrix multiplication 
@@ -55,7 +251,29 @@ void mult_mat_t_mat(double alpha,
     const double A[], 
     const double B[], 
     double X[], 
-    size_t m, size_t n, size_t k);
+    mwSize m, mwSize n, mwSize k);
+
+
+
+
+
+
+/********************************************
+* Debugging functions
+*
+*********************************************/
+
+
+/**
+Prints the contents of a matrix
+*/
+void print_matrix(const double A[], int m, int n, char* matrix_name);
+
+
+/**
+Prints the contents of a sparse vector
+*/
+void print_sparse_vector(const mxArray *A, char* vector_name);
 
 
 #endif 
