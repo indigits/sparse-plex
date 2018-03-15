ad024207449fcd79a44cd68b4bb16afc2ef443b3
Shailesh Kumar <shailesh@indigits.com>
2018-03-16 00:26:01 +0530

    comparison of matlab and C versions of OMP Chol

---
+++ examples/pursuit/fast/demo_compare_speed_omp_chol_mex.m
@@ -0,0 +1,31 @@
+m = 100;
+n = 1000;
+k = 12;
+A = spx.dict.simple.gaussian_dict(m, n);
+gen = spx.data.synthetic.SparseSignalGenerator(n, k);
+% create a sparse vector
+x =  gen.biGaussian();
+b = A*x;
+A  = double(A);
+result = spx.pursuit.fast_omp_chol(A, b, k, 1e-12);
+cmpare = spx.commons.SparseSignalsComparison(x, result, k);
+cmpare.summarize();
+
+% we now run both algorithms 100 times
+tic;
+for i=1:1000
+    result = spx.pursuit.fast_omp_chol(A, b, k, 1e-12);
+end
+elapsed_time1 = toc;
+fprintf('Time taken %0.4f seconds\n', elapsed_time1);
+
+tic;
+for i=1:1000
+    result = spx.pursuit.single.omp_chol(A, b, k, 1e-12);
+end
+elapsed_time2 = toc;
+fprintf('Time taken %0.4f seconds\n', elapsed_time2);
+
+improvement_factor = elapsed_time2 / elapsed_time1;
+fprintf('improvement_factor: %0.2f \n', improvement_factor);
+
