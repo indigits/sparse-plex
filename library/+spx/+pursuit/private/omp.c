d7fcd96d4a24653c2134ca3f96e8bcf9ef0a10e8
Shailesh Kumar <shailesh@indigits.com>
2018-03-25 09:33:23 +0530

    updates in implementation of OMP

--- library/+spx/+pursuit/private/omp.c
+++ library/+spx/+pursuit/private/omp.c
@@ -5,61 +5,18 @@
 #include "omp.h"
 #include "spxblas.h"
 
-#define CHOL_DEBUG 0
-
-void fill_vec_sparse_vals(const double values[],
-    const mwIndex indices[], double output[], 
-    mwSize n, mwSize k){
-    int j;
-    vec_set_value(output, 0, n);
-    for(j = 0; j < k; ++j){
-        output[indices[j]] = values[j];
-    }
-}
-
-
-int chol_update(const double m_subdict[],
-    const double v_atom[],
-    double m_lt[],
-    double v_b[],
-    double v_w[],
-    mwSize M,
-    mwSize k
-    ){
-    int j;
-    double d1;
-    // Incremental Cholesky decomposition
-    // b = D_I ' d
-    // note that k atoms means current atom is skipped.
-    mult_mat_t_vec(1, m_subdict, v_atom, v_b, M, k);
-    // L' w = b
-    lt_back_substitution(m_lt, v_b, v_w, M, k);
-    // Copy w to the new row of L
-    for (j=0; j < k; ++j){
-        m_lt[j*M + k] = v_w[j];
-    }
-    // Update the entry L[k, k]
-    d1 = 1 - inner_product(v_w, v_w, k);
-    if (d1 <= 1e-14){
-        // Selected atoms are dependent.
-        return -1;
-    }
-    m_lt[k*M + k] = sqrt(d1);    
-    return 0;
-}
 
 mxArray* omp_chol(const double m_dict[], 
     const double v_x[],
     mwSize M, 
     mwSize N,
-    mwSize K){
+    mwSize K, 
+    double res_norm_bnd){
 
     // List of indices of selected atoms
     mwIndex *selected_atoms = 0; 
     // Simple binary mask of selected atoms
     int* selected_atoms_mask = 0;   
-    // Approximation of x on selected atoms
-    double *v_x_hat = 0;
     // Storage for the Cholesky decomposition of D_I' D_I
     double *m_lt = 0;
     // The submatrix of selected atoms
@@ -81,10 +38,12 @@ mxArray* omp_chol(const double m_dict[],
     // Pointer to new atom
     const double* wv_new_atom;
     // residual norm squared
-    double res_nom_sqr;
+    double res_norm_sqr;
     /// Output array
     mxArray* p_alpha;
     double* v_alpha;
+    // square of upper bound on residual norm
+    double res_norm_bnd_sqr = SQR(res_norm_bnd);
 
     // counters
     int i, j , k;
@@ -101,7 +60,10 @@ mxArray* omp_chol(const double m_dict[],
     print_matrix(m_dict, M, N, "D");
     print_matrix(v_x, M, 1, "x");
 #endif
-
+    if (K < 0 || K > M) {
+        // K cannot be greater than M.
+        K = M;
+    }
     max_cols = (mwSize)(ceil(sqrt((double)M)/2.0) + 1.01);
     if(max_cols < K){
         max_cols = K;
@@ -111,8 +73,6 @@ mxArray* omp_chol(const double m_dict[],
     selected_atoms = (mwIndex*) mxMalloc(M*sizeof(mwIndex));
     // Total number of atoms is N
     selected_atoms_mask = (int*) mxMalloc(N*sizeof(int));
-    // approximation vector is in signal space R^M
-    v_x_hat = (double*) mxMalloc(M * sizeof(double));
     // Number of rows in L cannot exceed M. Number of columns 
     // cannot exceed max_cols.
     m_lt = (double*) mxMalloc(M*max_cols*sizeof (double));
@@ -134,7 +94,7 @@ mxArray* omp_chol(const double m_dict[],
 
 
     // Initialization
-    res_nom_sqr = inner_product(v_x, v_x, M);
+    res_norm_sqr = inner_product(v_x, v_x, M);
     //Compute proxy p  = D' * x
     mult_mat_t_vec(1, m_dict, v_x, v_proxy, M, N);
     // h = p = D' * r
@@ -147,7 +107,7 @@ mxArray* omp_chol(const double m_dict[],
     // Number of atoms selected so far.
     k = 0;
     // Iterate for each atom
-    while (k < K){
+    while (k < K &&  res_norm_sqr > res_norm_bnd_sqr){
         // Pick the index of (k+1)-th atom
         new_atom_index = abs_max_index(v_h, N);
 #if CHOL_DEBUG
@@ -199,7 +159,7 @@ mxArray* omp_chol(const double m_dict[],
         // Update h = D' r
         mult_mat_t_vec(1, m_dict, v_r, v_h, M, N);
         // Update residual norm squared
-        res_nom_sqr = inner_product(v_r, v_r, M);
+        res_norm_sqr = inner_product(v_r, v_r, M);
     }
 
     // Write the output vector
@@ -210,7 +170,6 @@ mxArray* omp_chol(const double m_dict[],
     // Memory cleanup
     mxFree(selected_atoms);
     mxFree(selected_atoms_mask);
-    mxFree(v_x_hat);
     mxFree(m_lt);
     mxFree(v_b);
     mxFree(v_w);
@@ -225,5 +184,6 @@ mxArray* omp_chol(const double m_dict[],
     // Return the result
     return p_alpha;
 }
+
 
 
