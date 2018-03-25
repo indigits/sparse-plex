ecc4851ce84db54097d263150db09cf8f57fa228
Shailesh Kumar <shailesh@indigits.com>
2018-03-25 09:36:19 +0530

    update in wrapper for fast OMP implementation using Cholesky

--- library/+spx/+pursuit/fast_omp_chol.m
+++ library/+spx/+pursuit/fast_omp_chol.m
@@ -1,3 +1,8 @@
-function result  = fast_omp_chol(Dict, x, K, epsilon)
-    result = mex_omp_chol(Dict, x, K, epsilon);
+function result  = fast_omp_chol(Dict, X, K, epsilon)
+    [M, S] = size(X);
+    N = size(Dict, 2);
+    result = zeros(N, S);
+    for s=1:S
+        result(:, s) = mex_omp_chol(Dict, X(:, s), K, epsilon);
+    end
 end
