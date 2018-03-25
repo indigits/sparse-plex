4ed35bfa209a82da2c1371169931b34d33405e53
Shailesh Kumar <shailesh@indigits.com>
2018-03-25 23:48:16 +0530

    Moving some reusable code.

--- library/+spx/+cluster/+ssc/+util/print_subspace_preservation_results.m
+++ library/+spx/+cluster/+ssc/+util/print_subspace_preservation_results.m
@@ -1,11 +1,11 @@
-function print_results(solver_name)
+function print_subspace_preservation_results(solver_name)
 filepath = sprintf('bin/subspace_preservation_test_%s.mat', solver_name);    
 load(filepath);
 true_labels = spx.cluster.labels_from_cluster_sizes(Ss);
 for r=1:R
     rho = rhos(r);
     cluster_labels = estimated_labels{r};
-    singular_values = estimated_singular_values{r};
+    %singular_values = estimated_singular_values{r};
     Z = estimated_coefficients{r};
     num_clusters = estimated_num_subspaces(r);
     comparsion_result = comparsion_results{r};
