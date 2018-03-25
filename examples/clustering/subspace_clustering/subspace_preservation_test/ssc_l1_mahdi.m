c742845b399b4f633e70274c6a0b4e152e61bd7f
Shailesh Kumar <shailesh@indigits.com>
2018-03-25 23:49:06 +0530

    Corrections in parameters

--- examples/clustering/subspace_clustering/subspace_preservation_test/ssc_l1_mahdi.m
+++ examples/clustering/subspace_clustering/subspace_preservation_test/ssc_l1_mahdi.m
@@ -1,4 +1,12 @@
-function result = ssc_l1_mahdi(X, D, K)
+function result = ssc_l1_mahdi(X, D, K, solver_params)
+% SSC_L1_MAHDI  Solve sparse subspace clustering using l1 minimization
+% method developed by Mahdi et al.
+%
+%   :X: Data set
+%   :D: Dimension for each subspace
+%   :K: Number of subspaces
+%   :solver_params: ignored.
+%
     options.num_clusters = K;
     result = spx.cluster.subspace.ssc_l1_mahdi(X, options);
 end
--- examples/clustering/subspace_clustering/subspace_preservation_test/ssc_nn_omp.m
+++ examples/clustering/subspace_clustering/subspace_preservation_test/ssc_nn_omp.m
@@ -1,4 +1,4 @@
-function result = ssc_nn_omp(X, D, K)
+function result = ssc_nn_omp(X, D, K, solver_params)
     solver = spx.cluster.ssc.SSC_NN_OMP(X, D, K);
     result = solver.solve();
 end
