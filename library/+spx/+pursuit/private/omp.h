8c76c1c80b19db81db3de61b9b27787bad611b94
Shailesh Kumar <shailesh@indigits.com>
2018-03-25 09:32:47 +0530

    declarations for batch omp and batch omp spr

--- library/+spx/+pursuit/private/omp.h
+++ library/+spx/+pursuit/private/omp.h
@@ -3,11 +3,71 @@
 
 #include "mex.h"
 
+#define CHOL_DEBUG 0
+#define ALLOC_REAL_ARR(n) (double*)mxMalloc((n)*sizeof(double))
+
+typedef struct {
+    // Dictionary
+    double* m_dict;
+    // Gram matrix
+    double* m_gram;
+    // Signal Matrix
+    double* m_x;
+    // D' X proxy matrix
+    double* m_dtx;
+    // Signal Space Dimension
+    mwSize M;
+    // Number of Atoms
+    mwSize N;
+    // Sparsity Level
+    mwSize K;
+    // Number of Signals
+    mwSize S;
+    // Residual Norm Bound
+    double res_norm_bnd;
+} BatchOMPInput;
+
+
+
+/**
+Computes the sparse representation of a vector x 
+in the dictionary D using the Orthogonal Matching
+Pursuit implemented with Cholesky update.
+
+Solves:  D alpha = x
+*/
 mxArray* omp_chol(const double m_dict[], 
     const double v_x[],
     mwSize M, 
     mwSize N,
-    mwSize K);
+    mwSize K, double res_norm_bnd);
+
+/**
+Computes the sparse representation of a set of vectors X
+in the dictionary D using the OMP implemented with
+Cholesky update and using the Gram matrix of the
+dictionary.
+
+Solve D A = X 
+
+with G = D' D available.
+
+*/
+mxArray* batch_omp_gram(const BatchOMPInput* in);
+
+
+
+/**
+Computes the subspace preserving
+representations of a dataset.
+*/
+mxArray* batch_omp_spr(double *m_dataset, // Dataset
+    mwSize M, // Data dimension
+    mwSize S, // Number of signals
+    mwSize K, // Sparsity level
+    double res_norm_bnd, // Residual norm bound
+    int sparse_output // Whether output is sparse matrix
+    );
 
 
 void fill_vec_sparse_vals(const double values[],
