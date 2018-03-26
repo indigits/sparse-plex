bd9679a42e7d0f205b865d32aa515251bfa07302
Shailesh Kumar <shailesh@indigits.com>
2018-03-26 15:21:22 +0530

    Moved these functions for reusability

---
+++ library/+spx/+cluster/+ssc/+util/bench_subspace_preservation.m
@@ -0,0 +1,87 @@
+function final_result = bench_subspace_preservation(solver, solver_name, solver_params)
+if nargin < 3
+    solver_params = struct;
+end
+% Create the directory for storing data
+spx.fs.ensure_dir('bin');
+
+% We will carry out one experiment for each signal density level
+setup.rhos = round(5.^[1:.4:5]);
+setup.rhos = round(5.^[1:.2:3.2]);
+
+% The following numbers have been picked up from the code provided by Chong You.
+setup.num_points_per_cluster_list = [30 55 98 177 320 577 1041 1880 3396 6132 11075 20000];
+%setup.num_points_per_cluster_list = [30 55 98 177 320 577];
+setup.num_points_per_cluster_list = [30 55 98 177 320 577 1041 1880 3396];
+%setup.num_points_per_cluster_list = [30 55];
+setup.rhos = setup.num_points_per_cluster_list / 6;
+disp(setup.rhos);
+% Number of experiments
+R= numel(setup.rhos);
+
+% Number of times each experiment should be run.
+setup.num_trials = 20;
+% store the overall experiment setup in a separate file.
+save(sprintf('bin/spx.cluster.ssc.util.bench_subspace_preservation_%s.mat', solver_name), 'setup');
+
+for r=1:R
+    rng(r);
+    for tt=1:setup.num_trials
+        % all data to be saved for a trial will be stored in a structure
+        trial.rho = setup.rhos(r);
+        % Trial number
+        trial.trial = tt;
+        % rho_index
+        trial.rho_id = r;
+        % Ambient space dimension
+        trial.M = 9;
+        % Number of subspaces
+        trial.K = 5;
+        % common dimension for each subspace
+        trial.D = 6;
+        % dimensions of each subspace
+        Ds = trial.D * ones(1, trial.K);
+        bases = spx.data.synthetic.subspaces.random_subspaces(trial.M, trial.K, Ds);
+        % Number of points on each subspace
+        trial.Sk = setup.num_points_per_cluster_list(r);
+        cluster_sizes = trial.Sk * ones(1, trial.K);
+        % total number of points
+        trial.S = sum(cluster_sizes);
+        fprintf('Trial %d, Points per subspace: %d, Total points: %d\n', tt, trial.Sk, trial.S);
+        % Let us generate uniformly distributed points in each subspace
+        points_result = spx.data.synthetic.subspaces.uniform_points_on_subspaces(bases, cluster_sizes);
+        X = points_result.X;
+        start_indices = points_result.start_indices;
+        end_indices = points_result.end_indices;
+        tstart = tic;
+        % Solve the sparse subspace clustering problem
+        clustering_result = solver(X, trial.D, trial.K, solver_params);
+        trial.elapsed_time = toc (tstart);
+        cluster_labels = clustering_result.labels;
+        % estimated number of clusters
+        trial.estimated_num_subspaces = clustering_result.num_clusters;
+        % true labels
+        true_labels = spx.cluster.labels_from_cluster_sizes(cluster_sizes);
+        % graph connectivity
+        trial.connectivity = spx.cluster.spectral.simple.connectivity(clustering_result.W, true_labels);
+        % Time to compare the clustering
+        comparsion_result = spx.cluster.clustering_error(cluster_labels, true_labels, trial.K);
+        trial.clustering_error_perc = comparsion_result.error_perc;
+        trial.clustering_acc_perc = 100 - comparsion_result.error_perc;
+        % Compute the statistics related to subspace preservation
+        spr_stats = spx.cluster.subspace.subspace_preservation_stats(clustering_result.Z, cluster_sizes);
+        trial.spr_error = spr_stats.spr_error;
+        trial.spr_flag = spr_stats.spr_flag;
+        trial.spr_perc = spr_stats.spr_perc;
+        fprintf('\nPoint density: %0.2f: , clustering error: %0.2f %% , clustering accuracy: %0.2f %%, mean spr error: %0.2f preserving : %0.2f %%, connectivity: %0.2f, elapsed time: %0.2f sec', trial.rho, trial.clustering_error_perc, trial.clustering_acc_perc, spr_stats.spr_error, spr_stats.spr_perc, trial.connectivity, trial.elapsed_time);
+        fprintf('\n\n');
+        filepath = sprintf('bin/spx.cluster.ssc.util.bench_subspace_preservation_%s_points=%d_trial=%d.mat', solver_name, trial.Sk, tt);
+        save(filepath, 'trial');
+    end
+end
+
+final_result = spx.cluster.ssc.util.merge_bench_subspace_preservation_results(solver_name);
+final_result.setup = setup;
+
+
+end
---
+++ library/+spx/+cluster/+ssc/+util/merge_bench_subspace_preservation_results.m
@@ -0,0 +1,79 @@
+function result = merge_results(solver_name)
+    pattern = sprintf('bin/bench_subspace_preservation_%s_*.mat', solver_name);
+    list = dir(pattern);
+    max_rho_id = 0;
+    num_trials = 0;
+    for i=1:numel(list)
+        fname = list(i).name;
+        fprintf('%s\n', fname);
+        filepath = fullfile('bin',  fname);
+        load(filepath);
+        num_trials= max(num_trials, trial.trial);
+        max_rho_id = max(max_rho_id, trial.rho_id);
+    end
+    fprintf('Number of trials: %d, Number of rhos: %d\n',  num_trials, max_rho_id);
+    result = struct;
+    result.M = trial.M;
+    result.K = trial.K;
+    result.D = trial.D;
+    result.elapsed_time_mat = zeros(num_trials, max_rho_id);
+    result.connectivity_mat = zeros(num_trials, max_rho_id);
+    result.clustering_error_perc_mat = zeros(num_trials, max_rho_id);
+    result.clustering_acc_perc_mat = zeros(num_trials, max_rho_id);
+    result.spr_error_mat = zeros(num_trials, max_rho_id);
+    result.spr_flag_mat = zeros(num_trials, max_rho_id);
+    result.spr_perc_mat = zeros(num_trials, max_rho_id);
+    num_points_mat = zeros(num_trials, max_rho_id);
+    result.trials = cell(num_trials, max_rho_id);
+    for i=1:numel(list)
+        fname = list(i).name;
+        fprintf('%s\n', fname);
+        filepath = fullfile('bin',  fname);
+        load(filepath);
+        r = trial.rho_id;
+        tt = trial.trial;
+        result.trials{tt, r} = trial;
+        result.elapsed_time_mat(tt, r) = trial.elapsed_time;
+        result.connectivity_mat(tt, r) = trial.connectivity;
+        result.clustering_error_perc_mat(tt, r) = trial.clustering_error_perc;
+        result.clustering_acc_perc_mat(tt, r) = trial.clustering_acc_perc;
+        result.spr_error_mat(tt, r) = trial.spr_error;
+        result.spr_flag_mat(tt, r) = trial.spr_flag;
+        result.spr_perc_mat(tt, r) = trial.spr_perc;
+        num_points_mat(tt, r) = trial.S;
+    end
+    result.num_points_arr = num_points_mat(1, :);
+    result.elapsed_time_arr = mean(result.elapsed_time_mat);
+    result.connectivity_arr = min(result.connectivity_mat);
+    result.clustering_error_perc_arr = mean(result.clustering_error_perc_mat);
+    result.clustering_acc_perc_arr = mean(result.clustering_acc_perc_mat);
+    result.spr_error_arr = mean(result.spr_error_mat);
+    result.spr_flag_arr = mean(result.spr_flag_mat);
+    result.spr_perc_arr = mean(result.spr_perc_mat);
+
+    fprintf('\n\n');
+    fprintf('Points: ');
+    fprintf('%d ', result.num_points_arr / 5);
+    fprintf('\n\n');
+    fprintf('Time: ');
+    fprintf('%.2f ', result.elapsed_time_arr);
+    fprintf('\n\n');
+    fprintf('Connectivity: ');
+    fprintf('%.2f ', result.connectivity_arr);
+    fprintf('\n\n');
+    fprintf('Clustering error(%%): ');
+    fprintf('%.2f ', result.clustering_error_perc_arr);
+    fprintf('\n\n');
+    fprintf('Clustering accuracy (%%): ');
+    fprintf('%.2f ', result.clustering_acc_perc_arr);
+    fprintf('\n\n');
+    fprintf('Subspace preserving representation error: ');
+    fprintf('%.4f ', result.spr_error_arr);
+    fprintf('\n\n');
+    fprintf('Subspace preserving representation flag: ');
+    fprintf('%.2f ', result.spr_flag_arr);
+    fprintf('\n\n');
+    fprintf('Subspace preserving representations(%%): ');
+    fprintf('%.2f ', result.spr_perc_arr);    
+    fprintf('\n\n');
+end
