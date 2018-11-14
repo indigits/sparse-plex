bb199e5c6e511b99084c03726758d3467f4c67b3
Shailesh Kumar <shailesh@indigits.com>
2018-11-14 16:06:52 +0530

    replacing the use of scale rows and cols with bsxfun

--- library/+spx/norm.m
+++ library/+spx/norm.m
@@ -152,31 +152,36 @@ classdef norm < handle
         function X = normalize_l1(X)
             % Normalizes all points in X by the column-wise l-1 norm
             norms = sum(abs(X));
-            X = spx.norm.scale_columns(X, norms);
+            X = bsxfun(@rdivide, X, norms + eps);
+            % X = spx.norm.scale_columns(X, norms);
         end
 
         function X = normalize_l1_rw(X)
             % Normalizes all points in X by the column-wise l-1 norm
             norms = sum(abs(X), 2);
-            X = spx.norm.scale_rows(X, norms);
+            X = bsxfun(@rdivide, X, norms + eps);
+            %X = spx.norm.scale_rows(X, norms);
         end
 
         function X = normalize_l2(X)
             % Normalizes all points in X by the column-wise l-2 norm
             norms =sqrt(sum(X .* conj(X), 1));
-            X = spx.norm.scale_columns(X, norms);
+            X = bsxfun(@rdivide, X, norms + eps);
+            %X = spx.norm.scale_columns(X, norms);
         end
 
         function X = normalize_l2_rw(X)
             % Normalizes all points in X by the row-wise l-2 norm
             norms =sqrt(sum(X .* conj(X), 2));
-            X = spx.norm.scale_rows(X, norms);
+            X = bsxfun(@rdivide, X, norms + eps);
+            % X = spx.norm.scale_rows(X, norms);
         end
 
         function X = normalize_linf(X)
             % Normalizes all points in X by the column-wise l-infinity norm
             norms = max(abs(X));
-            X = spx.norm.scale_columns(X, norms);
+            X = bsxfun(@rdivide, X, norms + eps);
+            %X = spx.norm.scale_columns(X, norms);
         end
 
         function X = scale_columns(X, factors)
