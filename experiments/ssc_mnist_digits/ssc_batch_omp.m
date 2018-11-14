6a2199bde2fc1c16d9388a647322b27fe4a20e28
Shailesh Kumar <shailesh@indigits.com>
2018-11-14 13:24:17 +0530

    script updates

--- experiments/ssc_mnist_digits/ssc_batch_omp.m
+++ experiments/ssc_mnist_digits/ssc_batch_omp.m
@@ -1,5 +1,7 @@
 function result = ssc_batch_omp(X, D, K, options)
-    solver = spx.cluster.ssc.SSC_BATCH_OMP(X, D, K);
+    rnorm_thr = 1e-3;
+    method = spx.cluster.ssc.OMP_REPR_METHOD.BATCH_FLIPPED_OMP_C;
+    solver = spx.cluster.ssc.SSC_OMP(X, D, K, rnorm_thr, method);
     solver.Quiet = true;
     result = solver.solve();
 end
