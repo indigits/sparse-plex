bfe34ec0fcd7c3262ea3400e52b8c54d9474bcc9
Shailesh Kumar <shailesh@indigits.com>
2018-11-15 11:15:13 +0530

    split experiment code

--- experiments/ssc_mnist_digits/ex_cmp_chong_our_ssc_omp_run.m
+++ experiments/ssc_mnist_digits/ex_cmp_chong_our_ssc_omp_run.m
@@ -1,33 +1,3 @@
-clc;
-close all;
-% initialize the random number generator for repeatability
-rng('default');
-digit_set = 0:9;
-num_samples_per_digit = 400;
-K = length(digit_set);
-% Number of subspaces
-K = K;
-cluster_sizes = num_samples_per_digit*ones(1, K);
-cluster_sizes = cluster_sizes;
-% maximum dimension for each subspace
-D = 10;
-S = sum(cluster_sizes);
-% identify sample indices for each digit
-sample_list = [];
-for k=1:K
-    digit = digit_set(k);
-    digit_indices = md.digit_indices(digit);
-    num_digit_samples = length(digit_indices);
-    choices = randperm(num_digit_samples, cluster_sizes(k));
-    selected_indices = digit_indices(choices);
-    % fprintf('%d ', selected_indices);
-    % fprintf('\n');
-    sample_list = [sample_list selected_indices];
-end
-[Y, true_labels] = md.selected_samples(sample_list);
-% disp(Y(1:10, 1)');
-% Perform PCA to reduce dimensionality
-Y = spx.la.pca.low_rank_approx(Y, 100);
 %Y = spx.norm.normalize_l2(Y);
 % Ambient space dimension and number of data points
 [M, S] = size(Y);
@@ -35,8 +5,9 @@ Y = spx.la.pca.low_rank_approx(Y, 100);
 rng('default');
 tstart = tic;
 rnorm_thr = 1e-3;
-method = spx.cluster.ssc.OMP_REPR_METHOD.FLIPPED_OMP_MATLAB;
 method = spx.cluster.ssc.OMP_REPR_METHOD.CLASSIC_OMP_C;
+method = spx.cluster.ssc.OMP_REPR_METHOD.BATCH_OMP_C;
+method = spx.cluster.ssc.OMP_REPR_METHOD.FLIPPED_OMP_MATLAB;
 solver = spx.cluster.ssc.SSC_OMP(Y, D, K, rnorm_thr, method);
 clustering_result = solver.solve();
 elapsed_time = toc (tstart);
@@ -50,7 +21,8 @@ spr_stats = spx.cluster.subspace.subspace_preservation_stats(clustering_result.Z
 spr_error = spr_stats.spr_error;
 spr_flag = spr_stats.spr_flag;
 spr_perc = spr_stats.spr_perc;
-fprintf('\nclustering error: %0.2f %% , clustering accuracy: %0.2f %%\n, mean spr error: %0.4f preserving : %0.2f %%\n, elapsed time: %0.2f sec',...
+fprintf('\nClustering time: %.2f, Representation time: %.2f\n', clustering_result.clustering_time, clustering_result.representation_time);
+fprintf('\nclustering error: %0.2f %% , clustering accuracy: %0.2f %%\n, mean spr error: %0.4f preserving : %0.2f %%, elapsed time: %0.2f sec',...
     clustering_error_perc, clustering_acc_perc,...
     spr_stats.spr_error, spr_stats.spr_perc,...
     elapsed_time);
---
+++ experiments/ssc_mnist_digits/ex_cmp_chong_our_ssc_omp_setup.m
@@ -0,0 +1,39 @@
+clc;
+close all;
+if ~exist('md', 'var')
+    clear all;
+    md = spx.data.image.ChongMNISTDigits;
+end
+% initialize the random number generator for repeatability
+rng('default');
+digit_set = 0:9;
+num_samples_per_digit = 600;
+K = length(digit_set);
+% Number of subspaces
+K = K;
+cluster_sizes = num_samples_per_digit*ones(1, K);
+cluster_sizes = cluster_sizes;
+% maximum dimension for each subspace
+D = 10;
+S = sum(cluster_sizes);
+% identify sample indices for each digit
+sample_list = [];
+for k=1:K
+    digit = digit_set(k);
+    fprintf('Loading samples for: %d\n', digit);
+    digit_indices = md.digit_indices(digit);
+    num_digit_samples = length(digit_indices);
+    choices = randperm(num_digit_samples, cluster_sizes(k));
+    selected_indices = digit_indices(choices);
+    % fprintf('%d ', selected_indices);
+    % fprintf('\n');
+    sample_list = [sample_list selected_indices];
+end
+[Y, true_labels] = md.selected_samples(sample_list);
+% disp(Y(1:10, 1)');
+% Perform PCA to reduce dimensionality
+fprintf('Performing PCA\n');
+tstart = tic;
+Y = spx.la.pca.low_rank_approx(Y, 100);
+elapsed_time = toc(tstart);
+fprintf('Time taken in computing PCA: %.2f seconds\n', elapsed_time);
