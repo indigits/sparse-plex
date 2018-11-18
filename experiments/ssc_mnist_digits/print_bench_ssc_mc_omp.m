4c6b0343708ca93c095eccf36d50c2e75765781e
Shailesh Kumar <shailesh@indigits.com>
2018-11-18 11:30:05 +0530

    SSC MC OMP data on MNIST

--- experiments/ssc_mnist_digits/print_bench_ssc_mc_omp.m
+++ experiments/ssc_mnist_digits/print_bench_ssc_mc_omp.m
@@ -3,7 +3,7 @@ configs{1} = create_config([1 1 1 1 1 1 1 1 1 1], 4, '1-4');
 configs{2} = create_config([2 1 1 1 1 1 1 1 1 1], 4, '2.1-4');
 configs{3} = create_config([4 2 1 1 1 1 1 1 1 1], 4, '42.1-4');
 configs{4} = create_config([2 2 2 2 2 2 2 2 2 2], 4, '2-4');
-num_samples_per_digit_arr = [20 30 40 50 60 70 80 90 100];
+num_samples_per_digit_arr = [50 80 100 150 200 300 400];
 
 %num_samples_per_digit_arr = [50 80 100 150 200 300 400];
 
@@ -21,7 +21,7 @@ for ns=1:nns
     for c=1:nc
         config = configs{c};
         solver_name = sprintf('ssc_mc_omp_%s', config.name);
-        filepath = sprintf('bin/solver_name_%s_digits=%d.mat', solver_name, num_samples_per_digit);
+        filepath = sprintf('bin/%s_digits=%d.mat', solver_name, num_samples_per_digit);
         load(filepath);
         results{ns, c} = result;
         clustering_acc_perc_mat(ns, c) = result.clustering_acc_perc;
@@ -40,3 +40,21 @@ disp(clustering_acc_perc_mat');
 
 fprintf('Subspace representation error: \n');
 disp(spr_error_mat');
+
+
+headers = {'Images per Digit'};
+for c=1:nc
+    headers{end+1} = configs{c}.name;
+end
+fprintf('Clustering accuracy a%%\n\n');
+data = [num_samples_per_digit_arr' clustering_acc_perc_mat];
+spx.io.rst.print_list_table(data, headers);
+
+fprintf('Subspace preserving representation error e%%\n\n');
+data = [num_samples_per_digit_arr' spr_error_mat*100];
+spx.io.rst.print_list_table(data, headers);
+
+
+fprintf('Time t\n\n');
+data = [num_samples_per_digit_arr' elapsed_time_mat];
+spx.io.rst.print_list_table(data, headers);
