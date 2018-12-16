6fafe908830432f647943316268a16654e2c9056
Shailesh Kumar <shailesh@indigits.com>
2018-12-16 22:25:44 +0530

    ssc mc benchmark update

--- experiments/ssc_mc_omp/bench_ssc_mc_omp.m
+++ experiments/ssc_mc_omp/bench_ssc_mc_omp.m
@@ -2,7 +2,7 @@ clear all;
 close all;
 clc;
 configs = cell(1, 5);
-configs{1} = create_config([1 1 1 1 1 1], 4, '1-4');
+configs{1} = create_config([1 1 1 1 1 1], 1, '1-1');
 configs{2} = create_config([2 1 1 1 1 1], 4, '2.1-4');
 configs{3} = create_config([4 2 1 1 1 1], 4, '42.1-4');
 configs{4} = create_config([2 2 2 2 2 2], 4, '2-4');
@@ -10,7 +10,9 @@ configs{5} = create_config([2 2 2 2 2 2], 8, '2-8');
 
 nc = numel(configs);
 
-for c=5:nc
+% We can control from here the configurations for which the script will be run.
+% It is possible to run the script one by one for each configuration.
+for c=1:2
     config = configs{c};
     if isempty(config)
         continue;
@@ -18,6 +20,6 @@ for c=5:nc
     solver_name = sprintf('ssc_mc_omp_%s', config.name);
     result = spx.cluster.ssc.util.bench_subspace_preservation(@ssc_mc_omp, solver_name, config);
     result.config = config;
-    filepath = sprintf('bin/solver_%s.mat', solver_name);
+    filepath = sprintf('bin/%s.mat', solver_name);
     save(filepath, 'result');
 end
