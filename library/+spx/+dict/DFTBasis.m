4fdd558c7c83811670d47eb1e73d614f2f58dfb9
Shailesh Kumar <shailesh@indigits.com>
2019-01-27 16:27:25 +0530

    DCT and DFT basis operators

---
+++ library/+spx/+dict/DFTBasis.m
@@ -0,0 +1,54 @@
+classdef DFTBasis < spx.dict.Operator 
+
+properties(SetAccess=private)
+    % Dimensions
+    N
+end
+
+methods
+
+function self = DFTBasis(N)
+    if nargin < 1
+        error('Basis dimensions must be specified');
+    end
+    self.N = N;
+end
+
+function [mm, nn]  = get_size(self)
+    mm = self.N;
+    nn = self.N;
+end
+
+function result = apply(self, vectors)
+    if size(vectors, 1) ~= self.N
+        error('Dimensions mismatch');
+    end
+    result = ifft(vectors) * sqrt(self.N);
+end
+
+function result = apply_ctranspose(self, vectors)
+    if size(vectors, 1) ~= self.N
+        error('Dimensions mismatch');
+    end
+    result = fft(vectors) / sqrt(self.N);
+end
+
+function result = double(self)
+    % Converts the operator into a MATRIX
+    n = self.N;
+    result = zeros(n);
+    for i=1:n
+        x = zeros(n, 1);
+        x(i) = 1;
+        result(:, i) = self.apply(x);
+    end
+end
+
+
+function result = norm(self)
+    result = 1;
+end
+
+end
+
+end
