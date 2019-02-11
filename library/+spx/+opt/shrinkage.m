9126e69577c130735b42179e71e1df62af12ebe2
Shailesh Kumar <shailesh@indigits.com>
2019-02-11 16:58:13 +0530

    faster implementation

--- library/+spx/+opt/shrinkage.m
+++ library/+spx/+opt/shrinkage.m
@@ -1,4 +1,6 @@
 function y = shrinkage(a, kappa)
-    y = max(0, a-kappa) - max(0, -a-kappa);
+% Performs element wise shrinkage
+    % y = max(0, a-kappa) - max(0, -a-kappa);
+    y = max(0, a-kappa) + min(0, a+kappa);
 end
 
