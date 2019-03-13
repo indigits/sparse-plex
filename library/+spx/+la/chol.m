43c86b9815fc94bace52ba02cea4af5bdb936786
Shailesh Kumar <shailesh@indigits.com>
2019-03-13 21:55:28 +0530

    reformatting

--- library/+spx/+la/chol.m
+++ library/+spx/+la/chol.m
@@ -2,18 +2,23 @@ classdef chol
 
 methods(Static)
 
-    function L_new = chol_update(L, atom)
-        n = size(L, 1);
-        if n == 0
-            L_new = sqrt(atom);
-            return;
-        end
-        v = atom(1:end-1);
-        c = atom(end);
-        opts.LT = true;
-        w = linsolve(L, v, opts);
-        c_new = sqrt(c - w' * w);
-        L_new = [L zeros(n, 1); w' c_new];
+function L_new = chol_update(L, atom)
+    n = size(L, 1);
+    if n == 0
+        L_new = sqrt(atom);
+        return;
     end
-end
-end
+    v = atom(1:end-1);
+    c = atom(end);
+    opts.LT = true;
+    w = linsolve(L, v, opts);
+    c_new = sqrt(c - w' * w);
+    L_new = [L zeros(n, 1); w' c_new];
+end % function
+
+
+
+
+end % methods 
+
+end % classdef
