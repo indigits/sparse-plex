f32b4cb895d59ada7ff44806622cfc8cc0bea13f
Shailesh Kumar <shailesh@indigits.com>
2019-05-31 12:54:56 +0530

    A function which measures the orhtogonality of a basis

--- library/+spx/la.m
+++ library/+spx/la.m
@@ -2,6 +2,13 @@ classdef la
 
 methods(Static)
 
+function no = nonorthogonality(U)
+    % Computes the deviation from orthogonality of a basis
+    [m, k] = size(U);
+    gap = U' * U - eye(k);
+    no = norm(gap);
+end
+
 
 end % methods
 
