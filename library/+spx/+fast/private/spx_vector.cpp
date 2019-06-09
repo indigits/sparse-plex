7bb2334e64df3965719f3111dc525afc813844f0
Shailesh Kumar <shailesh@indigits.com>
2019-06-09 23:16:49 +0530

    improvements in vector class

--- library/+spx/+fast/private/spx_vector.cpp
+++ library/+spx/+fast/private/spx_vector.cpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <cstdlib>
 #include "spx_vector.hpp"
 #include "spxblas.h"
 #include "blas.h"
@@ -124,7 +125,23 @@ Vec::~Vec() {
 
 }
 
-mwIndex Vec::abx_max_index() const {
+double Vec::abs_max(mwSignedIndex start, mwSignedIndex end) const{
+    if (end < 0) {
+        end = m_n;
+    }
+    if (start < 0) {
+        throw std::invalid_argument("start is invalid");
+    }
+    if (start > end) {
+        throw std::invalid_argument("start is greater than end.");
+    }
+    mwSignedIndex  incx = m_inc;
+    mwSignedIndex nn = end - start;
+    mwSignedIndex maxIndex = idamax(&nn, m_pVec + start * incx, &incx) - 1;
+    return fabs(m_pVec[maxIndex*incx]);
+}
+
+mwIndex Vec::abs_max_index() const {
     mwSignedIndex  nn = m_n;
     mwSignedIndex  incx = m_inc;
     return idamax(&nn, m_pVec, &incx) - 1;
@@ -202,7 +219,19 @@ Vec& Vec::operator = (const Vec &o) {
     return *this;
 }
 
-void Vec::elt_wise_square() {
+void Vec::set(const index_vector& indices, const double& value){
+    size_t n = indices.size();
+    for (int i = 0; i < n; ++i){
+        mwIndex index = indices[i];
+        if (index >= m_n){
+            throw std::length_error("Vec.set:: Out of range index.");
+        }
+        m_pVec[index * m_inc] = value;
+    }
+}
+
+
+Vec& Vec::square() {
     mwSize n = m_n;
     mwSignedIndex inc = m_inc;
     double* x = m_pVec;
@@ -210,9 +239,10 @@ void Vec::elt_wise_square() {
         *x = SQR(*x);
         x += inc;
     }
+    return *this;
 }
 
-void Vec::elt_wise_square(Vec& out) const {
+void Vec::square(Vec& out) const {
     mwSize n = std::min(m_n, out.m_n);
     mwSignedIndex x_inc = m_inc;
     const double* x = m_pVec;
@@ -225,7 +255,7 @@ void Vec::elt_wise_square(Vec& out) const {
     }
 }
 
-void Vec::elt_wise_inv() {
+Vec& Vec::inv() {
     mwSize n = m_n;
     mwSignedIndex inc = m_inc;
     double* x = m_pVec;
@@ -239,7 +269,31 @@ void Vec::elt_wise_inv() {
         }
         x += inc;
     }
+    return *this;
 }
+
+Vec& Vec::abs(){
+    mwSize n = m_n;
+    mwSignedIndex inc = m_inc;
+    double* x = m_pVec;
+    for (mwIndex i = 0 ; i < n; ++i) {
+        *x = fabs(*x);
+        x += inc;
+    }
+    return *this;
+}
+
+Vec& Vec::scale(double value){
+    mwSize n = m_n;
+    mwSignedIndex inc = m_inc;
+    double* x = m_pVec;
+    for (mwIndex i = 0 ; i < n; ++i) {
+        *x = (*x) * value;
+        x += inc;
+    }
+    return *this;
+}
+
 
 double Vec::norm_squared() const {
     mwSize n = m_n;
@@ -298,17 +352,67 @@ double Vec::inner_product(const Vec& o) const {
 }
 
 
-void Vec::print(const std::string& name) const {
+void Vec::print(const std::string& name, mwSignedIndex end, bool scientific) const {
     if (name.size() > 0) {
-        mexPrintf("\n%s = \n\n", name.c_str());
+        mexPrintf("%s = ", name.c_str());
     }
     int n = m_n;
+    if (end >= 0) {
+        n = end;
+    }
     double* x = m_pVec;
     for (int i = 0; i < n; ++i) {
-        mexPrintf("   %lf", *x);
+        if (scientific){
+            mexPrintf("   %e", *x);
+        }else{
+            mexPrintf("   %.4lf", *x);
+        }
         x += m_inc;
+    }
+    if (end < m_n){
+        mexPrintf(" ...");
     }
     mexPrintf("\n");    
 }
+
+void flags_to_indices(const b_vector& flags, index_vector& indices, int n){
+    size_t m = flags.size();
+    if (n > 0){
+        m = std::min(m, (size_t) n);
+    }
+    // identify how many columns are selected
+    n = 0;
+    for (int i= 0; i < m; ++i){
+        if (flags[i]){
+            n += 1;
+        }
+    }
+    // allocate space for those many integer indices
+    indices.resize(n);
+    int j = 0;
+    // iterate over flags and identify the indices of selected columns
+    for (int i=0; i < m; ++i){
+        if (flags[i]){
+            indices[j] = i;
+            ++j;
+        }
+    }
+}
+
+void indices_to_flags(const index_vector& indices, b_vector& flags){
+    size_t m = flags.size();
+    for (int i=0; i <m; ++i){
+        flags[i] = false;
+    }
+    size_t n = indices.size();
+    for (int i=0; i < n; ++i){
+        size_t index = indices[i];
+        if (index >= m){
+            continue;
+        }
+        flags[index] = true;
+    }
+}
+
 
 }
--- library/+spx/+fast/private/spx_vector.hpp
+++ library/+spx/+fast/private/spx_vector.hpp
@@ -13,12 +13,16 @@ namespace spx {
 typedef std::vector<double> d_vector;
 //! A vector of integer values
 typedef std::vector<int> i_vector;
+//! A vector of unsigned integer values
 typedef std::vector<size_t> index_vector;
+//! A vector of boolean values
+typedef std::vector<bool> b_vector;
 
 //! Square all elements of a vector in place
 void square_inplace(d_vector& v);
 void square(const d_vector& src, d_vector& dst);
 
+//! Converts a double vector to an MX Array
 mxArray* d_vec_to_mx_array(const d_vector& x);
 
 void print_d_vec(const d_vector& x, const std::string& name = "");
@@ -94,6 +98,9 @@ public:
   //! Destructor
   ~Vec();
 public:
+/*****************************************************************************
+ *  Basic setters and getters
+ *****************************************************************************/
   //! Returns the length of the vector
   inline mwSize length() const {
     return m_n;
@@ -118,30 +125,54 @@ public:
   inline double& operator[](mwIndex index) {
     return m_pVec[index * m_inc];
   }
-  //! Returns the index of largest entry by magnitude in the vector
-  mwIndex abx_max_index() const;
-  //! Return the index of largest entry in the vector
-  mwIndex max_index() const;
-  //! Return the value and index of largest entry in the vector
-  void max_index(double& value, mwIndex& index) const;
+
+ /*****************************************************************************
+ *  Extraction operations
+ *****************************************************************************/
+
   //! Extract a subset of values from the vector
   void extract(const mwIndex indices[], Vec& out, mwSize k=0) const;
+  //! Square all the entries element wise and write in another vector
+  void square(Vec& out) const;
+
+
+ /*****************************************************************************
+ *  Assignment and self manipulation operations. Can be chained.
+ *****************************************************************************/
   //! Set all values in the vector to a fixed value
   Vec& operator=(const double& value);
   //! Copy assignment operator
   Vec& operator = (const Vec &o);
+  //! Set a value to a subset of indices
+  void set(const index_vector& indices, const double& value);
   //! Square all entries in the vector
-  void elt_wise_square();
-  //! Square all the entries element wise and write in another vector
-  void elt_wise_square(Vec& out) const;
+  Vec& square();
   //! Invert all entries in the vector
-  void elt_wise_inv();
+  Vec& inv();
+  //! Compute absolute value of all entries in the vector
+  Vec& abs();
+  //! Scale each value by a factor
+  Vec& scale(double value);
+ /*****************************************************************************
+ *  Summaries, statistics
+ *****************************************************************************/
+  //! Returns the absolute maximum of a range of values
+  double abs_max(mwSignedIndex start = 0, mwSignedIndex end = -1) const;
+  //! Returns the index of largest entry by magnitude in the vector
+  mwIndex abs_max_index() const;
+  //! Return the index of largest entry in the vector
+  mwIndex max_index() const;
+  //! Return the value and index of largest entry in the vector
+  void max_index(double& value, mwIndex& index) const;
   //! Return the squared norm of the vector
   double norm_squared() const;
   //! Return the norm of the vector
   inline double norm() const {
     return sqrt(norm_squared());
   }
+ /*****************************************************************************
+ *  Vector - Vector operations
+ *****************************************************************************/
   //! add another vector into this vector
   void add(const Vec& o, double alpha=1);
   //! subtract another vector
@@ -150,8 +181,11 @@ public:
   void multiply(const Vec& o);
   //! Inner product with another vector
   double inner_product(const Vec& o) const;
+ /*****************************************************************************
+ *  Debugging support
+ *****************************************************************************/
   //! print the contents of the vector
-  void print(const std::string& name = "") const;
+  void print(const std::string& name = "", mwSignedIndex end = -1, bool scientific = false) const;
 private:
   // //! Disable copy constructor
   // Vec(const Vec &t);
@@ -163,6 +197,42 @@ private:
   //! Step size between vector elements
   mwIndex m_inc;
 };
+
+
+template<typename T>
+T square(const T& value){
+  return value*value;
+}
+
+template <typename T> int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
+
+/**
+Computes sqrt(a^2 + b^2) but is careful to avoid overflow.
+*/
+template<typename T>
+T pythag(T a, T b){
+  // signs of a and b
+  int sa = sgn(a);
+  int sb = sgn(b);
+  // Find the absolute max value between a and b
+  T rmax = std::max(sa*a, sb*b);
+  if (rmax == T(0)){
+    return rmax;
+  }
+  // Bring both a and b to be below 1.
+  a = a/rmax;
+  b = b/rmax;
+  // compute the hypotenuse and scale it back
+  return rmax*sqrt(a*a + b*b);
+}
+
+//! Converts a vector of boolean flags to indices
+void flags_to_indices(const b_vector& flags, index_vector& indices, int n = -1);
+
+//! Converts a vector of indices to boolean flags
+void indices_to_flags(const index_vector& indices, b_vector& flags);
 
 
 }
