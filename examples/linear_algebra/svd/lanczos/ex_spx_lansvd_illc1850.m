189aeb709455fc89700f977585a82d6f5fbae248
Shailesh Kumar <shailesh@indigits.com>
2019-07-31 12:46:24 +0530

    multiple trials

--- examples/linear_algebra/svd/lanczos/ex_spx_lansvd_illc1850.m
+++ examples/linear_algebra/svd/lanczos/ex_spx_lansvd_illc1850.m
@@ -4,18 +4,24 @@ clearvars;
 cd I:\work\projects\sparse-plex\library\+spx\+fast\private;
 make mex_lansvd.cpp;
 cd I:\work\projects\sparse-plex\examples\linear_algebra\svd\lanczos;
-A = mmread('illc1850.mtx');
-k = 4;
+A = spx.data.mtx_mkt.illc1850;
+k = 20;
+trials  = 50;
 fprintf('Running SPX fast LAN SVD with k=%d\n', k);
 tstart = tic;
+for i=1:trials
 s = svds(A, k);
+end
 svds_time = toc(tstart);
 fprintf('svds singular values: ');
 spx.io.print.vector(s(1:k));
 options.verbosity = 0;
+options.k = k;
 % options.max_iters = 80;
 tstart = tic;
-[U, S, V, details] = spx.fast.lansvd(A, k, options);
+for i=1:trials
+[U, S, V, details] = spx.fast.lansvd(A, options);
+end
 lansvd_time = toc(tstart);
 fprintf('lansvd singular values: ');
 spx.io.print.vector(S);
